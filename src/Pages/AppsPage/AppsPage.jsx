89d1b2e27f3158017fb390dd1def313dc0fec439
tfshorifulislam <tfshorifulislam@gmail.com>
2026-04-10 16:03:59 +0600

    navbar done

---
+++ src/Pages/AppsPage/AppsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AppsPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Apps Page</h1>
+        </div>
+    );
+};
+
+export default AppsPage;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import HomePage from './Pages/HomePage/HomePage'
+import MainPage from './mainPage/MainPage'
+import AppsPage from './Pages/AppsPage/AppsPage'
+import InstallationPage from './Pages/InstallationPage/InstallationPage'
+
+const route = createBrowserRouter([
+  {
+    path: '/', element: <MainPage />,
+    children: [
+      { path: '/', element: <HomePage /> },
+      { path: '/apps', element: <AppsPage /> },
+      { path: '/installation', element: <InstallationPage /> }
+    ]
+  }
+])
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route} />
   </StrictMode>,
 )
